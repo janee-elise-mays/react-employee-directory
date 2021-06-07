1e00c4391628d8a24356f5a8f39b373506901c09
Janeé Mays <janee.elise@gmail.com>
2021-06-07 19:54:00 -0400

    moved bracket to render Directory

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -10,7 +10,6 @@ class Directory extends Component {
         sort: [],
         sorted: false,
     }
-}
 
 componentDidMount = () => {
     API.getRandomEmployees().then((res) => {
@@ -23,24 +22,25 @@ componentDidMount = () => {
 render = () => {
     return (
         <div>
-        <Find name="search" startSort={this.startSort} label="Search" />
-        <div className="container-fluid">
-             {/* https://react-bootstrap.github.io/components/table/ */}
-             <table striped bordered hover>
-                 <thread className="thread">
-                     <tr>
-                         <th>Image</th>
-                         <th>Name</th>
-                         <th>Email</th>
-                         <th>Phone</th>
-                         <th>Date of Birth</th>
-                     </tr>
-                 </thread>
-             </table>
-         </div>
-         </div>
-    )
+            <Find name="search" startSort={this.startSort} label="Search" />
+            <div className="container-fluid">
+                {/* https://react-bootstrap.github.io/components/table/ */}
+                <table striped bordered hover>
+                    <thread className="thread">
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Date of Birth</th>
+                        </tr>
+                    </thread>
+                </table>
+            </div>
+        </div>
+    );
 }
+};
 
 
 export default Directory;
