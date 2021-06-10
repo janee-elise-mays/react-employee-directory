309f0b0ccbd474accc441cf606e4b6daf351e8de
Janeé Mays <janee.elise@gmail.com>
2021-06-09 21:03:26 -0400

    adding sort/search features

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Employees from "./Employees";
 import API from "../utils/API";
 import Table from "react-bootstrap/Table";
-import Headeer from "./Header";
 
  class Directory extends Component {
      state = {
@@ -22,7 +21,24 @@ import Headeer from "./Header";
             this.setState({employees: res.data.results})})
     } 
 
-    
+    sortNames = () => {
+        let { names, search } = this.state;
+        let nameSort = names.filter((sorted) => {
+            return (
+                sorted.name.first ||
+                sorted.name.last ||
+                sorted.email
+            );
+        });
+        this.setState({ nameSort });
+    }
+
+    startSort = (event) => {
+        this.setState({ search: event.target.value }, () => {
+            this.sortNames();
+            this.setState({ sorted: true });
+        });
+    };
 
 
     render = () => {
@@ -40,7 +56,10 @@ import Headeer from "./Header";
                          <th>Date of Birth</th>
                      </tr>
                  <tbody>
-                 {this.state.employees.map(employee => (
+                 {
+                 !this.state.sorted
+            /* JavaScript conditional operator condition ? true : false. */
+                 ? this.state.employees.map(employee => (
                      <Employees
                      key ={employee.login.uuid}
                      firstName={employee.name.first}
@@ -50,7 +69,19 @@ import Headeer from "./Header";
                      picture={employee.picture.large}
                      dob={employee.dob.date} 
                      />
-                 ))}
+                 ))
+                 : this.state.nameSort.map((employee) => (
+                    <Employees
+                     key ={employee.login.uuid}
+                     firstName={employee.name.first}
+                     lastName={employee.name.last}
+                     phone={employee.phone}
+                     email={employee.email}
+                     picture={employee.picture.large}
+                     dob={employee.dob.date} 
+                     />
+                 ))
+                 }
                  </tbody>
                  </thread>
              </Table>
